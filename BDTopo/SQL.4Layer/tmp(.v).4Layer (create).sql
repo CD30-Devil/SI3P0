1f548053490cc4d03f01c124618acb2a2d573478
Michaël Galien <michael.galien@gard.fr>
2022-02-18 15:37:14 +0100

    Création d'un couche de voies ferrées agrégées pour une meilleure représentation carto

--- BDTopo/SQL.4Layer/tmp(.v).4Layer (create).sql
+++ BDTopo/SQL.4Layer/tmp(.v).4Layer (create).sql
@@ -17,3 +17,13 @@ create view tmp.D30RN_4Layer as
 select cpx_numero as "Numero", ST_LineMerge(ST_Collect(Geometrie)) as Geom
 from pc.d30_bdtopo_troncon_de_nationale
 group by cpx_numero;
+
+create view tmp.D30VoieFerreeAgregee_4Layer as
+with Agregat as (
+    select Nature, (ST_Dump(ST_LineMerge(ST_Collect(t.geometrie)))).Geom
+    from d.bdtopo_troncon_de_voie_ferree t
+    inner join v.Gard g on ST_Intersects(t.geometrie, g.Geom)
+    group by Nature
+)
+select Nature as "Nature", ST_3DLength(Geom) as "Longueur", Geom
+from Agregat;
--- BDTopo/SQL.4Layer/tmp(.v).4Layer (drop).sql
+++ BDTopo/SQL.4Layer/tmp(.v).4Layer (drop).sql
@@ -2,3 +2,4 @@
 drop view if exists tmp.D30Autoroute_4Layer;
 drop view if exists tmp.RN_4Layer;
 drop view if exists tmp.D30RN_4Layer;
+drop view if exists tmp.D30VoieFerreeAgregee_4Layer;
