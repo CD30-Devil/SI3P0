d8eb8b7551999df89b489f5c1f0bb9238e0b5b00
Michaël Galien <michael.galien@gard.fr>
2022-05-10 22:37:52 +0200

    Modif requête de recherche des mairies
    Suite à la remontée d'un ré-utilisateur, modification de la requête de recherches des mairies.

--- Cartes décoratives/Vues/Atlas.sql
+++ Cartes décoratives/Vues/Atlas.sql
@@ -9,5 +9,5 @@ select
     ST_Centroid(zai.geometrie) as geometrie
 from zone_d_activite_ou_d_interet zai
 inner join commune c on ST_Intersects(c.geometrie, zai.geometrie)
-where nature = 'Mairie'
-and (nature_detaillee is null or nature_detaillee = 'Hôtel de ville');
+where nature_detaillee = 'Hôtel de ville' or (nature = 'Mairie' and nature_detaillee is null)
+order by 1;
