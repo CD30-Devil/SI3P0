a79c631deabc04c63ac2dde485edeb6e05f9d4ab
Michaël Galien <michael.galien@gard.fr>
2022-09-23 16:58:09 +0200

    refactoring...remise à plat

--- BDTopo/SQL.4Layer/4Layer (create).sql
+++ BDTopo/SQL.4Layer/4Layer (create).sql
@@ -1,24 +1,27 @@
-﻿create view tmp.Autoroute_4Layer as
+﻿-- schémas spécifiques SI3P0 (tmp = temporaire, m = données)
+set search_path to tmp, d, public;
+
+create view Autoroute_4Layer as
 select cpx_numero as "Numero", ST_LineMerge(ST_Collect(Geometrie)) as Geom
 from bdtopo_troncon_d_autoroute
 group by cpx_numero;
 
-create view tmp.D30Autoroute_4Layer as
+create view D30Autoroute_4Layer as
 select cpx_numero as "Numero", ST_LineMerge(ST_Collect(Geometrie)) as Geom
 from d30_bdtopo_troncon_d_autoroute
 group by cpx_numero;
 
-create view tmp.RN_4Layer as
+create view RN_4Layer as
 select cpx_numero as "Numero", ST_LineMerge(ST_Collect(Geometrie)) as Geom
 from bdtopo_troncon_de_nationale
 group by cpx_numero;
 
-create view tmp.D30RN_4Layer as
+create view D30RN_4Layer as
 select cpx_numero as "Numero", ST_LineMerge(ST_Collect(Geometrie)) as Geom
 from d30_bdtopo_troncon_de_nationale
 group by cpx_numero;
 
-create view tmp.D30VoieFerreeAgregee_4Layer as
+create view D30VoieFerreeAgregee_4Layer as
 with Agregat as (
     select Nature, (ST_Dump(ST_LineMerge(ST_Collect(t.geometrie)))).Geom
     from d30_bdtopo_troncon_de_voie_ferree t
--- BDTopo/SQL.4Layer/4Layer (drop).sql
+++ BDTopo/SQL.4Layer/4Layer (drop).sql
@@ -1,5 +1,8 @@
-﻿drop view if exists tmp.Autoroute_4Layer;
-drop view if exists tmp.D30Autoroute_4Layer;
-drop view if exists tmp.RN_4Layer;
-drop view if exists tmp.D30RN_4Layer;
-drop view if exists tmp.D30VoieFerreeAgregee_4Layer;
+﻿-- schémas spécifiques SI3P0 (tmp = temporaire)
+set search_path to tmp, public;
+
+drop view if exists Autoroute_4Layer;
+drop view if exists D30Autoroute_4Layer;
+drop view if exists RN_4Layer;
+drop view if exists D30RN_4Layer;
+drop view if exists D30VoieFerreeAgregee_4Layer;
