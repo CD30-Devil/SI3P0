8c8aaf0daccb89df7af673a28d9c83c03664987f
Michaël Galien <michael.galien@gard.fr>
2022-11-28 09:24:22 +0100

    cleabs index unique

--- BDTopo/SQL/index (create).sql
+++ BDTopo/SQL/index (create).sql
@@ -1,7 +1,7 @@
 ﻿-- schémas spécifiques SI3P0 (d = données)
 set search_path to d, public;
 
-create index if not exists bdtopo_departement_code_insee on bdtopo_departement (code_insee);
+create unique index if not exists bdtopo_departement_code_insee on bdtopo_departement (code_insee);
 create index if not exists bdtopo_departement_geometrie on bdtopo_departement using gist (geometrie);
 
 create unique index if not exists bdtopo_troncon_de_route_cleabs on bdtopo_troncon_de_route (cleabs);
